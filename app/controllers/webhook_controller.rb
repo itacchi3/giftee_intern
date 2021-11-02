ddae3cb2ae5883405518b0fefd5e64dbed00055c
itaitoshiki <toshiki.i0918@gmail.com>
2021-11-02 16:09:36 +0900

    個人チャットの場合一つのメッセージごとに採点

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -25,46 +25,57 @@ class WebhookController < ApplicationController
         case event.type
         when Line::Bot::Event::MessageType::Text
           message_text = event.message['text']
-          group = Group.find_or_create_by!(group_id: event['source']['groupId'])
-          group_score_calc_set = GroupScoreCalcSet.find_or_create_by!(group_id: group.group_id )
+          if event['source']['groupId']
+            group = Group.find_or_create_by!(group_id: event['source']['groupId'])
+            group_score_calc_set = GroupScoreCalcSet.find_or_create_by!(group_id: group.group_id )
 
-          if message_text == "/スタート" && !group.is_measurement_period
-            group.is_measurement_period = true
-            group.save!
-            group_score_calc_set.set_id += 1
-            group_score_calc_set.save
-            message = {
-              type: 'text',
-              text: "計測開始"
-            }
-            client.reply_message(event['replyToken'], message)
-          elsif message_text == "/ストップ" && group.is_measurement_period
-            group.is_measurement_period = false
-            group.save!
-            user_id_list = UserScore.where(group_id: group.group_id, set_id: group_score_calc_set.set_id).pluck(:user_id).uniq
+            if message_text == "/スタート" && !group.is_measurement_period
+              group.is_measurement_period = true
+              group.save!
+              group_score_calc_set.set_id += 1
+              group_score_calc_set.save
+              message = {
+                type: 'text',
+                text: "計測開始"
+              }
+              client.reply_message(event['replyToken'], message)
+            elsif message_text == "/ストップ" && group.is_measurement_period
+              group.is_measurement_period = false
+              group.save!
+              user_id_list = UserScore.where(group_id: group.group_id, set_id: group_score_calc_set.set_id).pluck(:user_id).uniq
 
-            reply_text = "結果発表"
-            max_score = 0
-            user_id_list.each { |user_id|
-              user_name = JSON.parse(client.get_group_member_profile(group.group_id, user_id).body)["displayName"]
-              scores = UserScore.where(group_id: group.group_id, set_id: group_score_calc_set.set_id, user_id: user_id).pluck(:score)
-              score_ave = scores.sum.fdiv(scores.length).to_i
+              reply_text = "結果発表"
+              max_score = 0
+              user_id_list.each { |user_id|
+                user_name = JSON.parse(client.get_group_member_profile(group.group_id, user_id).body)["displayName"]
+                scores = UserScore.where(group_id: group.group_id, set_id: group_score_calc_set.set_id, user_id: user_id).pluck(:score)
+                score_ave = scores.sum.fdiv(scores.length).to_i
 
-              reply_text += "\n#{user_name}さん: #{score_ave}点"
-            }
+                reply_text += "\n#{user_name}さん: #{score_ave}点"
+              }
 
-            message = {
-              type: 'text',
-              text: reply_text
-            }
-            client.reply_message(event['replyToken'], message)
-          else
-            if group.is_measurement_period
-              google_cloud_language_client = GoogleCloudLanguageClient.new
-              response = google_cloud_language_client.analyze_sentiment(text: event.message['text'])
-              score = response.document_sentiment.score.to_f.round(1) * 100
-              UserScore.create!(group_id: event['source']['groupId'], set_id: group_score_calc_set.set_id, user_id: event['source']['userId'], score: score)
+              message = {
+                type: 'text',
+                text: reply_text
+              }
+              client.reply_message(event['replyToken'], message)
+            else
+              if group.is_measurement_period
+                google_cloud_language_client = GoogleCloudLanguageClient.new
+                response = google_cloud_language_client.analyze_sentiment(text: event.message['text'])
+                score = response.document_sentiment.score.to_f.round(1) * 100
+                UserScore.create!(group_id: event['source']['groupId'], set_id: group_score_calc_set.set_id, user_id: event['source']['userId'], score: score)
+              end
             end
+          else
+            google_cloud_language_client = GoogleCloudLanguageClient.new
+            response = google_cloud_language_client.analyze_sentiment(text: message_text)
+            score = response.document_sentiment.score.to_f.round(1) * 100
+            message = {
+              type: 'text',
+              text: "ポジティブ度: #{score.to_i}点"
+            }
+            client.reply_message(event['replyToken'], message)
           end
 
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
